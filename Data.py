11e1333d06bfd9d2de9c16f07f091c517d846a1d
GooseXP <zka719@gmail.com>
2021-08-10 08:35:02 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -9,8 +9,8 @@ from shutil import rmtree
 from subprocess import Popen, PIPE, STDOUT, TimeoutExpired
 
 DISCOUNT = 0.99
-REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
-MIN_REPLAY_MEMORY_SIZE = 1000  # Minimum number of steps in a memory to start training
+REPLAY_MEMORY_SIZE = 50000  # How many last steps to keep for model training
+MIN_REPLAY_MEMORY_SIZE = 5000  # Minimum number of steps in a memory to start training
 MINIBATCH_SIZE = 1000  # How many steps (samples) to use for training
 UPDATE_TARGET_EVERY = 5  # Update Target model after x steps
 MEMORY_FRACTION = 0.20
