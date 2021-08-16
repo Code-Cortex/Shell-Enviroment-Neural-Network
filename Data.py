c09c0e56b38de74cdb2d9986b130a6c0a7f997df
GooseXP <zka719@gmail.com>
2021-08-16 07:29:37 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -11,7 +11,7 @@ from subprocess import Popen, PIPE, STDOUT, TimeoutExpired
 DISCOUNT = 0.99
 REPLAY_MEMORY_SIZE = 50000  # How many last steps to keep for model training
 MIN_REPLAY_MEMORY_SIZE = 5000  # Minimum number of steps in a memory to start training
-MINIBATCH_SIZE = 500 # How many steps (samples) to use for training
+MINIBATCH_SIZE = 100 # How many steps (samples) to use for training
 UPDATE_TARGET_EVERY = 5  # Update Target model after x steps
 
 # Exploration settings
